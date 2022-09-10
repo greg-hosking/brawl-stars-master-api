0f8311061b30911143dde24851f899b595920f47
Greg Hosking <hoskinggregory@gmail.com>
2022-09-09 21:50:34 -0400

    Add headers to Lambda response

--- index.ts
+++ index.ts
@@ -4,12 +4,18 @@ import {
   Handler,
 } from 'aws-lambda';
 
+import { brawlers } from './brawlers';
+
 export const handler: Handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResultV2> => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda! 123'),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(brawlers),
   };
   return response;
 };
